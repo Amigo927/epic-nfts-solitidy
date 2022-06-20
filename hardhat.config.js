3f7c35bc298a5c92f906c751efa6ad9477e72948
Amitani Taihei <amitani.taihei@gmail.com>
2022-06-20 15:01:06 +0900

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require('@nomiclabs/hardhat-waffle');
+require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-etherscan");
+module.exports = {
+  solidity: '0.8.1',
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  }
+};
